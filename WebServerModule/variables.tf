7b1c45d0afa43a1b570368d47b57fa683b6df65c
Las-Mohamed <airscal@gmail.com>
2023-06-15 08:25:09 +0000

    v2

---
+++ WebServerModule/variables.tf
@@ -0,0 +1,17 @@
+variable "instance_size" {
+   type = string
+   description = "Azure instance size"
+   default = "Standard_F2"
+}
+
+variable "location" {
+   type = string
+   description = "Region"
+   default = "France Central"
+}
+
+variable "environment" {
+   type = string
+   description = "Environment"
+   default = "Staging"
+}
